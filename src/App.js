4816ef19092d61a453e8345e9a4eb6f996b4a57b
Wilmer Alvarez <wilmerap@gmail.com>
2020-11-09 21:19:22 -0500

    Importacion de Antd y React Router DOM

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import React, { useState } from "react";
+import { Layout } from "antd";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 
