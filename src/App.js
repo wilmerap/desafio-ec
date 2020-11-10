f610f3592d4d61ed9a22f252c13d4901f462230d
Wilmer Alvarez <wilmerap@gmail.com>
2020-11-10 13:40:03 -0500

    Creacion de URL con React Router

--- src/App.js
+++ src/App.js
@@ -11,23 +11,26 @@ import Datos from "./pages/Datos";
 import Confirmacion from "./pages/Confirmacion";
 
 function App() {
+  const { Content } = Layout;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Layout>
+        <Router>
+          <Content>
+                <Switch>
+                  <Route path="/suscripcion" exact={true}>
+                    <Suscripcion />
+                  </Route>
+                  <Route path="/datos" exact={true}>
+                    <Datos />
+                  </Route>
+                  <Route path="/confirmacion" exact={true}>
+                    <Confirmacion />
+                  </Route>
+                </Switch>
+          </Content>
+        </Router>
+      </Layout>
   );
 }
 
